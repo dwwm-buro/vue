ede0718c4009c233ea8a9ad69e332f8baf0a3b20
Matthieu Mota <matthieumota@gmail.com>
2023-08-17 15:33:09 +0200

    Emit

--- 02-exercices/src/App.vue
+++ 02-exercices/src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
+import Button from './components/Button.vue';
 import Presentation from './components/Presentation.vue';
-
+import { ref } from 'vue';
+const total = ref(0);
+const handle = (event) => total.value += event;
 </script>
 
 <template>
     <div class="container">
         <Presentation />
+        {{ total }}
+        <Button @incrementBy="handle">Envoyer</Button>
+        <Button color="blue" @incrementBy="handle">Commander</Button>
+        <Button color="red" @incrementBy="handle">Voir</Button>
     </div>
 </template>
 
---
+++ 02-exercices/src/components/Button.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+defineProps(['color']);
+const emit = defineEmits(['incrementBy']);
+
+const count = ref(0);
+const increment = () => {
+    count.value++;
+    emit('incrementBy', 1);
+}
+</script>
+
+<template>
+    <button @click="increment" :class="color" :style="{ backgroundColor: color }">
+        <slot /> {{ count }}
+    </button>    
+</template>
+
+<style scoped>
+button {
+    padding: 10px 15px;
+    border: 0;
+    border-radius: 20px;
+    cursor: pointer;
+    background-color: #000;
+    color: #fff;
+}
+
+button.blue {
+    background-color: blue !important;
+    color: #fff;
+}
+</style>
