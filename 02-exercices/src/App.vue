102dc71abafcccc09d05f0e93c090d3d357e2abc
Matthieu Mota <matthieumota@gmail.com>
2023-08-17 17:26:50 +0200

    Exo counter

--- 02-exercices/src/App.vue
+++ 02-exercices/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Button from './components/Button.vue';
+import Counter from './components/Counter.vue';
 import Presentation from './components/Presentation.vue';
 import { ref } from 'vue';
 const total = ref(0);
@@ -9,10 +10,19 @@ const handle = (event) => total.value += event;
 <template>
     <div class="container">
         <Presentation />
+
+        <h2>Exemple avec le emit</h2>
         {{ total }}
         <Button @incrementBy="handle">Envoyer</Button>
         <Button color="blue" @incrementBy="handle">Commander</Button>
         <Button color="red" @incrementBy="handle">Voir</Button>
+
+        <h2>TP : Composant Counter</h2>
+
+        <Counter />
+        <Counter :start="12" :max="20" />
+        <Counter :start="4" :max="20" />
+        <Counter :start="17" :max="30" />
     </div>
 </template>
 
---
+++ 02-exercices/src/components/Counter.vue
@@ -0,0 +1,46 @@
+<script setup>
+const props = defineProps(['start', 'max']);
+import { ref } from 'vue';
+
+const count = ref(props.start || 0);
+const decrement = () => count.value--;
+const increment = () => count.value++;
+const color = computed(() => {
+    if (count.value >= max) {
+        return 'red';
+    } else {
+        return '';
+    }
+})
+</script>
+
+<template>
+    <div class="flex" :style="{ backgroundColor: color }">
+        <button @click="decrement" :disabled="count <= 0">-</button>
+        <span>{{ count }}</span>
+        <button @click="increment" v-if="!max || count < max">+</button>
+    </div>
+</template>
+
+<style scoped>
+.flex {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    justify-content: center;
+    margin-bottom: 20px;
+    max-width: 200px;
+}
+
+button {
+    background-color: aqua;
+    border: 0;
+    padding: 10px;
+    cursor: pointer;
+    border-radius: 25px;
+}
+
+button:disabled {
+    opacity: 0.25;
+}
+</style>
